de2c11175f313c17e5b43dae4ef2f01c8393f341
fredtigersoft <47736961+fredtigersoft@users.noreply.github.com>
2019-03-07 18:19:05 +0200

    adding third part library : lodash

--- src/pages/team-detail/team-detail.ts
+++ src/pages/team-detail/team-detail.ts
@@ -21,34 +21,34 @@ export class TeamDetailPage {
     this.team = this.navParams.data;
 
     this.tourneyData = this.eliteApi.getCurrentTourney();
-    // this.games = _.chain(this.tourneyData.games)
-    //             .filter(g => g.team1Id === this.team.id || g.team2Id === this.team.id)
-    //             .map(g =>{
-    //               let isTeam1 = (g.team1Id === this.team.id);
-    //               let opponentName = isTeam1 ? g.team2 : g.team1;
-    //               let scoreDisplay = this.getScoreDisplay(isTeam1, g.team1Score, g.team2Score);
-    //               return{
-    //                 gameId : g.id,
-    //                 opponent : opponentName,
-    //                 time : Date.parse(g.time),
-    //                 location : g.location,
-    //                 locationUrl : g.locationUrl,
-    //                 scoreDisplay : scoreDisplay,
-    //                 homeAway : (isTeam1 ? "vs." : "at")
-    //               };
-    //             })
-    //             .value();
+    this.games = _.chain(this.tourneyData.games)
+                .filter(g => g.team1Id === this.team.id || g.team2Id === this.team.id)
+                .map(g =>{
+                  let isTeam1 = (g.team1Id === this.team.id);
+                  let opponentName = isTeam1 ? g.team2 : g.team1;
+                  let scoreDisplay = this.getScoreDisplay(isTeam1, g.team1Score, g.team2Score);
+                  return{
+                    gameId : g.id,
+                    opponent : opponentName,
+                    time : Date.parse(g.time),
+                    location : g.location,
+                    locationUrl : g.locationUrl,
+                    scoreDisplay : scoreDisplay,
+                    homeAway : (isTeam1 ? "vs." : "at")
+                  };
+                })
+                .value();
   }
 
-  // getScoreDisplay(isTeam1, team1Score, team2Score){
-  //     if(team1Score && team2Score){
-  //       var teamScore = (isTeam1 ? team1Score : team2Score);
-  //       var opponentScore = (isTeam1 ? team2Score : team1Score);
-  //       var winIndicator = teamScore > opponentScore ?  "W: " : "L: ";
-  //       return winIndicator + teamScore + "_" + opponentScore;
-  //     }else{
-  //       return "";
-  //     }
-  // }
+  getScoreDisplay(isTeam1, team1Score, team2Score){
+      if(team1Score && team2Score){
+        var teamScore = (isTeam1 ? team1Score : team2Score);
+        var opponentScore = (isTeam1 ? team2Score : team1Score);
+        var winIndicator = teamScore > opponentScore ?  "W: " : "L: ";
+        return winIndicator + teamScore + "_" + opponentScore;
+      }else{
+        return "";
+      }
+  }
 
 }

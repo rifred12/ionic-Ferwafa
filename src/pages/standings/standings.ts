bde21335011d3a7f11f5e5673c8c619d8c403fcc
fredtigersoft <47736961+fredtigersoft@users.noreply.github.com>
2019-03-08 18:33:14 +0200

    standings

--- src/pages/standings/standings.ts
+++ src/pages/standings/standings.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the StandingsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { EliteApi } from '../../providers/elite-api/elite-api';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'page-standings',
@@ -14,11 +9,30 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class StandingsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  public allStandings : any[];
+  public standings: any[];
+  public team : any;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private eliteApi : EliteApi,
+    ) { }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad StandingsPage');
+    this.team = this.navParams.data;
+    let tourneyData = this.eliteApi.getCurrentTourney();
+    this.standings = tourneyData.standings;
+
+    this.allStandings = 
+    _.chain(this.standings)
+    .groupBy('divisioon')
+    .toPairs()
+    .map(item => _.zipObject(['divisionName','divisionStandings'], item))
+    .value();
+
+    console.log('standings', this.standings);
+    console.log('division Standings', this.allStandings);
   }
 
 }

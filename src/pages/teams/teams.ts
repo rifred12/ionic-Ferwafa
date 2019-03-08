2da20852a72a60629c650dde501a17aa48155972
fredtigersoft <47736961+fredtigersoft@users.noreply.github.com>
2019-03-08 12:37:42 +0200

    division category on team list

--- src/pages/teams/teams.ts
+++ src/pages/teams/teams.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { TeamHomePage } from '../team-home/team-home';
 import { EliteApi } from '../../providers/elite-api/elite-api';
-
+import * as _ from 'lodash';
 
 
 @Component({
@@ -12,22 +12,48 @@ import { EliteApi } from '../../providers/elite-api/elite-api';
 export class TeamsPage {
 
   public teams = [];
-
+  private allTeams: any;
+  private allTeamDivisions: any;
 
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
-    private eliteApi : EliteApi) {
+    private eliteApi: EliteApi,
+    private loadingController: LoadingController) {
   }
 
   ionViewDidLoad() {
     let selectedTourney = this.navParams.data;
-    this.eliteApi.getTournamentData(selectedTourney.id).subscribe(data => {
-      this.teams = data.teams;
+
+    let loader = this.loadingController.create({
+      content: 'Getting data...'
     });
+
+    loader.present().then(() => {
+
+      this.eliteApi.getTournamentData(selectedTourney.id).subscribe(data => {
+        this.allTeams = data.teams;
+
+        this.allTeamDivisions =
+          _.chain(data.teams)
+            .groupBy('division')
+            .toPairs()
+            .map(item => _.zipObject(['divisionName', 'divisionTeams'], item))
+            .value();
+
+        this.teams = this.allTeamDivisions;
+        console.log('division Teams', this.teams);
+
+        loader.dismiss();
+      });
+
+
+    });
+
   }
 
-  itemTapped($event, team){
+
+  itemTapped($event, team) {
     this.navCtrl.push(TeamHomePage, team)
   }
 
